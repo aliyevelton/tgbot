25fce0d2c6092ac332dd0d581a588c493e94fae3
Elton Aliyev <68150154+aliyevelton@users.noreply.github.com>
2021-07-02 15:13:20 +0400

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1823852396:AAG4U7QJqJ48u1rzTR0HbJy75ILXEvr68G8"
+    OWNER_ID = "1064219453"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "aliyevelton"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -34,8 +34,8 @@ class Config(object):
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
+    BAN_STICKER = 'CAACAgQAAxkBAAEChP9g3vShkpioMTlZwEA28VB3xtgSkgACEQEAAoIVwR3BpZegGLqqMCAE'  # banhammer marie sticker
+    ALLOW_EXCL = True  # Allow ! commands as well as /
 
 
 class Production(Config):
