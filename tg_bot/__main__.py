55e1954c809c65bab6297e457ba795c398d03be7
Elton Aliyev <68150154+aliyevelton@users.noreply.github.com>
2021-07-02 15:18:39 +0400

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,22 +19,13 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Salam, {}, Mənim adım Niyaməddindir! 
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find what makes me tick [here](github.com/PaulSonOfLars/tgbot)!
-
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
-
-You can find the list of available commands with /help.
-
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+/help yazaraq komandalarla tanış ola bilərsən.
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
+Hello There! Mənim adım *Niyaməddin*dir.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 
